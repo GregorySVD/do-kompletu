3baaf5876a210064c60b7943c9a6128b3d8480e1
OverDoxeE <westeros7748@gmail.com>
2026-08-08 15:56:48 +0200

    Initialize FastAPI backend

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
